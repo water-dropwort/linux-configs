f84abc6914500d9a74b7be572aaef1bcc06307dc
water-dropwort <57586990+water-dropwort@users.noreply.github.com>
2023-09-09 20:30:17 +0900

    add company-mode

--- configs/.emacs.d/init.el
+++ configs/.emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(platformio-mode flycheck ccls lsp-mode markdown-mode zenburn-theme)))
+   '(company platformio-mode flycheck ccls lsp-mode markdown-mode zenburn-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -92,10 +92,13 @@
 ;; C++ mode config
 (add-hook 'c++-mode-hook (lambda ()
                            (electric-indent-local-mode -1)
-                           (lsp)
+                           (lsp-deferred)
                            (platformio-conditionally-enable)))
 
 ;; C mode config
 (add-hook 'c-mode-hook   (lambda ()
                            (lsp-deferred)
                            (platformio-conditionally-enable)))
+
+;; COMPANY MODE
+(add-hook 'after-init-hook 'global-company-mode)
